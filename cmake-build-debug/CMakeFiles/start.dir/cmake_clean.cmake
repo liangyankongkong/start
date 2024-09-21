4b0a29b886a716cd66198b64187fc623012842af
ltt <ltt0300@163.com>
2024-09-21 19:50:21 +0800

    fixed

---
+++ cmake-build-debug/CMakeFiles/start.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/start.dir/main.cpp.obj"
+  "CMakeFiles/start.dir/main.cpp.obj.d"
+  "libstart.dll.a"
+  "start.exe"
+  "start.exe.manifest"
+  "start.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/start.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
